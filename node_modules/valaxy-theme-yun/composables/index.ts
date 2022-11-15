6f17343ab741f7371106ce5d4c7522e33a7fbdb3
Yongheng Liu <yongheng.liu@nx-engine.com>
2022-11-15 12:56:59 +0800

    new blog

--- node_modules/valaxy-theme-yun/composables/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './config'
-export * from './helper'
-export * from './post'
--- node_modules/valaxy-theme-yun/utils/index.ts
+++
@@ -1,5 +0,0 @@
-export const anonymousImage = 'https://cdn.yunyoujun.cn/img/avatar/none.jpg'
-
-export const onImgError = (e: Event) => {
-  (e.target as HTMLImageElement).src = anonymousImage
-}
