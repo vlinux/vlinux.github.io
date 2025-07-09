ff8293b052e2c3856d067b802539196eb7dde3f8
Yongheng Liu <yongheng.liu@nx-engine.com>
2025-07-09 09:17:58 +0800

    更新

--- assets/YunGallery-dae03831.js
+++
@@ -1,1 +0,0 @@
-import{h as n,g as t,o as s,c as l}from"./app-3e08b1b2.js";const c=n({__name:"YunGallery",props:{photos:null},setup(o){return(a,p)=>{const e=t("VAGallery");return s(),l(e,{photos:o.photos},null,8,["photos"])}}});export{c as default};
---
+++ assets/_...all_-770c6f24.js
@@ -0,0 +1,1 @@
+import{h as a,a2 as s,a3 as n,a4 as r,i as c,o as p}from"./app-1dca60bf.js";import{b as o}from"./route-block-83d24a4e.js";const u=a({__name:"[...all]",setup(i){const t=s(),e=n();return r(()=>{e.path!=="/"&&e.path.endsWith("/")&&t.replace(e.path.slice(0,-1))}),(f,l)=>(p(),c("div"))}});typeof o=="function"&&o(u);export{u as default};
